6111a90e381d84d5edda7b92f55b4d4e420828cf
Christopher Chew <fishkepr@comcast.net>
2018-02-17 18:29:30 -0800

    Created Sandbox area for testing code snippets

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-function helloCodeFellows(){
-  console.log('Executing heloCodeFellows');
-  return('EXIT');
-
+function Dog (name, age) {
+  this.name = name;
+  this.age = age;
 }
-var testAlpha = helloCodeFellows();
-var testBeta = helloCodeFellows;
-console.log('testAlpha: '+testAlpha);
-console.log('testBeta: '+testBeta);
+var ageDiff = function(dog1, dog2) {
+  return Math.abs(dog1.age - dog2.age);
+};
+ageDiff(new Dog('Sparky', 12), new Dog('Molly', 8));
